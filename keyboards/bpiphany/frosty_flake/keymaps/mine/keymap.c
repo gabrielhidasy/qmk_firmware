6a903a96c5f546f3cbc8cfe774406b3c69d82d13
Gabriel Hidasy Rezende <gabrielhidasy@google.com>
2019-06-05 11:47:23 -0300

    Fix some bugs in the layout

--- keyboards/bpiphany/frosty_flake/keymaps/mine/keymap.c
+++ keyboards/bpiphany/frosty_flake/keymaps/mine/keymap.c
@@ -115,19 +115,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT(\
     TD(ALLS), TD(EMOJIS), TD(ANIMAL), TD(ETC), TD(FOODS), KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     KC_PSCR, KC_SLCK, RESET, \
     KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_MS_BTN3, KC_MS_BTN3, KC_TRNS, KC_A, KC_A, KC_A, KC_A, \
-    KC_RCTRL, KC_MPRV, KC_MPLY, KC_MNXT, LCTL(KC_R), KC_T, KC_CALC, KC_PGUP, KC_HOME, KC_PGDN, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_MS_BTN1, KC_MS_BTN2, KC_TRNS, KC_A, KC_A, KC_A, KC_A, KC_A, \
-    KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  \
-    MO(2), KC_MENU, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+    KC_RCTRL, KC_MPRV, KC_MPLY, KC_MNXT, LCTL(KC_R), KC_T, KC_CALC, KC_PGUP, KC_HOME, KC_PGDN, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_MS_BTN1, KC_MS_BTN2, KC_TRNS, KC_A, KC_A, KC_A, KC_A, \
+    KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+    MO(2), KC_APP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_END, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_UP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_TRNS, KC_TRNS \
   ),
-  [2] = LAYOUT(\
-    KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     KC_PSCR, KC_SLCK, KC_PAUS,                                        \
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,    KC_MPRV, KC_MPLY, KC_MNXT,    KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,    KC_MUTE, KC_VOLD, KC_VOLU,    KC_P7,   KC_P8,   KC_P9,   KC_PPLS, \
-    KC_LCTL, KC_D,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                                   KC_P4,   KC_P5,   KC_P6,            \
-    KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,             KC_MS_U,             KC_P1,   KC_P2,   KC_P3,   KC_PENT, \
-    KC_BTN1, KC_BTN3, KC_BTN2,                            KC_SPC,                             KC_RALT, KC_RGUI, TG(2),   KC_NO,      KC_MS_L, KC_MS_D, KC_MS_R,    KC_P0,            KC_PDOT           \
-  ),
+  /* [2] = LAYOUT(\
+     KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     KC_PSCR, KC_SLCK, KC_PAUS,                                        \
+     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,    KC_MPRV, KC_MPLY, KC_MNXT,    KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, \
+     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,    KC_MUTE, KC_VOLD, KC_VOLU,    KC_P7,   KC_P8,   KC_P9,   KC_PPLS, \
+     KC_LCTL, KC_D,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                                   KC_P4,   KC_P5,   KC_P6,            \
+     KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,             KC_MS_U,             KC_P1,   KC_P2,   KC_P3,   KC_PENT, \
+     KC_BTN1, KC_BTN3, KC_BTN2,                            KC_SPC,                             KC_RALT, KC_RGUI, TG(2),   KC_NO,      KC_MS_L, KC_MS_D, KC_MS_R,    KC_P0,            KC_PDOT           \
+   ), */
 };
 
 LEADER_EXTERNS();
