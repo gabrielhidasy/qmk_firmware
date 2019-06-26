edcbf15d6a49f6e3707218e19bad4aeebe1007dd
Gabriel Hidasy Rezende <gabrielhidasy@google.com>
2019-06-26 09:29:03 -0300

    Even more changes, I should have used a branch for that

--- keyboards/bpiphany/frosty_flake/keymaps/mine/config.h
+++ keyboards/bpiphany/frosty_flake/keymaps/mine/config.h
@@ -2,7 +2,8 @@
 
 // place overrides here
 #define TAPPING_TERM 200
-#define LEADER_TIMEOUT 800
+#define LEADER_TIMEOUT 300
+#define LEADER_PER_KEY_TIMING
 
 #define DISABLE_SPACE_CADET_ROLLOVER
 
@@ -17,3 +18,5 @@
 #define MOUSEKEY_MAX_SPEED      7
 #define MOUSEKEY_WHEEL_DELAY    0
 #define RETRO_TAPPING
+#define DEBOUNCE 20
+#define DEBOUNCE_DELAY 100
