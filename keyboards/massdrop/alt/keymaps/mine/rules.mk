0063e8ee81544ab250238fb8b9cba513bb144f2a
Gabriel Hidasy Rezende <gabrielhidasy@google.com>
2019-06-26 09:29:44 -0300

    Add my Alt keymap and the mdloader binary

---
+++ keyboards/massdrop/alt/keymaps/mine/rules.mk
@@ -0,0 +1,14 @@
+# This keymap requires Massdrop Configurator support
+OPT_DEFS += -DUSE_MASSDROP_CONFIGURATOR
+BOOTMAGIC_ENABLE = yes       # Virtual DIP switch configuration(+1000)
+MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
+EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
+CONSOLE_ENABLE = no         # Console for debug(+400)
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+TAP_DANCE_ENABLE = yes
+LEADER_ENABLE = yes
+
+
+
+
