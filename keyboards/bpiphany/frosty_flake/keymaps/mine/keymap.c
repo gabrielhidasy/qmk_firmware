b8ef831a2cd213c71e13ccfb8fb8f702da6f9520
Gabriel Hidasy Rezende <gabrielhidasy@gmail.com>
2019-09-22 18:37:09 -0300

    Improve QMK firmware for Frosty Flake

--- keyboards/bpiphany/frosty_flake/keymaps/mine/keymap.c
+++ keyboards/bpiphany/frosty_flake/keymaps/mine/keymap.c
@@ -24,7 +24,8 @@ enum taps{
   HAND,
   MEMES,
   COPA,
-  ALLS
+  ALLS,
+  TD_HOME_END
 };
 
 enum unicode_name {
@@ -59,7 +60,8 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   [ETC] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(cycleEtc, NULL, NULL, 800),
   //[VEHICLES] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(cycleVehicles, NULL, NULL, 800),
   //[SUPPLEMENT] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(cycleSupplement, NULL, NULL, 800),
-  [ALLS] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(cycleAll, NULL, NULL, 800)
+  [ALLS] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(cycleAll, NULL, NULL, 800),
+  [TD_HOME_END] = ACTION_TAP_DANCE_DOUBLE(KC_HOME, KC_END),
 // Other declarations would go here, separated by commas, if you have them
 };
 
@@ -106,7 +108,7 @@ const uint32_t PROGMEM unicode_map[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(\
     KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SLCK, KC_PAUS, \
-    KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_INS, KC_HOME, KC_PGUP, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, \
+    KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_INS, TD_HOME_END, KC_PGUP, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, \
     MT(MOD_LCTL,KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, KC_END, KC_PGDN, KC_P7, KC_P8, KC_P9, KC_PPLS, \
     LT(1,KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,  KC_ENT,     KC_P4, KC_P5, KC_P6, \
     KC_LSPO,  KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,   KC_RSPC,    KC_UP,    KC_P1, KC_P2, KC_P3, KC_PENT, \
